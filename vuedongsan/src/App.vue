f95abd037d18c49f91c86cf5300d1608e9d52123
imminpie <mjkang0715@gmail.com>
2023-12-08 16:40:17 +0900

    feat: v-if 와 모달창 구현

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,11 +1,20 @@
 <template>
+  <div class="black-bg" v-if="modal === true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지내용임</p>
+      <button @click="isModal">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <!-- v-for='작명 in 몇회' :key='작명' -->
     <a v-for="menu_items in menu" :key="menu_items" href="#">{{ menu_items }}</a>
   </div>
 
   <div v-for="(products_items, idx) in products" :key="products_items">
-    <h4 :style="orange">{{ products_items }}</h4>
+    <img :src="require(`@/assets/room${idx}.jpg`)" alt="room" class="room-img" />
+    <h4 @click="isModal" :style="orange">{{ products_items }}</h4>
     <p>50 만원</p>
     <button @click="increase(idx)">허위매물신고</button>
     <span>신고수 : {{ count[idx] }}</span>
@@ -21,11 +30,15 @@ export default {
       menu: ['Home', 'Shop', 'About'],
       orange: 'color:orange',
       count: [0, 0, 0],
+      modal: false,
     };
   },
   methods: {
     increase(idx) {
       this.count[idx] += 1;
+    },
+    isModal() {
+      this.modal = !this.modal;
     },
   },
   components: {},
@@ -40,6 +53,25 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 .menu {
   background: darkslateblue;
   padding: 15px;
@@ -49,5 +81,9 @@ export default {
   color: white;
   padding: 10px;
   text-decoration: none;
+}
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>

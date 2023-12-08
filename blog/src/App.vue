95f9facae5c9d69b26438e178660edbd8098a8db
imminpie <mjkang0715@gmail.com>
2023-12-09 00:45:39 +0900

    feat: 블로그 글목록 구현

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,11 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Vuelog</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <h5>React 개발자의 블로그입니다.</h5>
+    <p>- Vue 로 만들었음 -</p>
+  </div>
+
+  <ListItem :posts="posts" />
 </template>
 
 <script>
+import ListItem from './components/ListItem.vue';
+import posts from './assets/posts.js';
 export default {
   name: 'App',
-  components: {},
+  data() {
+    return {
+      posts: posts,
+    };
+  },
+  components: {
+    ListItem,
+  },
 };
 </script>
 
---
+++ blog/src/components/ListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-for="posts_items in posts" :key="posts_items">
+    <h5>{{ posts_items.title }}</h5>
+    <p>{{ posts_items.date }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    posts: Array,
+  },
+};
+</script>
+<style></style>

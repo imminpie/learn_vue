fd6dfe230ac9e0f1756656d3826e7ae1294dfb99
imminpie <mjkang0715@gmail.com>
2023-12-09 14:13:06 +0900

    feat: 뷰 인스타그램 데이터바인딩

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-for="(posts_items, idx) in posts" :key="posts_items">
+    <Post :posts="posts[idx]" />
+  </div>
+</template>
+<script>
+import Post from './Post.vue';
+export default {
+  //eslint-disable-next-line
+  name: 'Container',
+  props: {
+    posts: Array,
+  },
+  components: {
+    Post,
+  },
+};
+</script>
+<style></style>
